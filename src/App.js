4b689386f2aa2c5d155791de2737591a7928a800
ilias elmakhfi <iliaselmakhfi@gmail.com>
2021-01-06 05:04:04 +0100

    Implementation de MARKDOWN APP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import marked from 'marked'
+
+import {sampleText} from './sampleText'
+
+class App extends Component {
+
+  state = {
+    text : sampleText
+  }
+
+  handleArea = event => {
+    const text = event.target.value
+    this.setState({text})
+  }
+
+  handleText = text =>  {
+    const __html = marked(text, {sanitize: true})
+    return {__html}
+  }
+
+
+  render(){
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='.col-lg-6'>
+            <textarea defaultValue={this.state.text} onChange={this.handleArea} className='form-control' rows='35'/>
+          </div>
+          <div className='.col-lg-6'>
+            <div dangerouslySetInnerHTML={this.handleText(this.state.text)}>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
